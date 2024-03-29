859cb8fecb9f338afb9cb2751f182e80a821f447
slvg01 <151031347+slvg01@users.noreply.github.com>
2024-03-29 14:55:45 +0100

    day 5 output

--- README_vf.md
+++ README_vf.md
@@ -1,3 +1,47 @@
-# 7_wiwino
+# Objective Wiwino market analysis
 
-Selection critere avec le code  mais selection finale manuelle 
+> [Wiwino](https://www.sqlite.org/index.html) is a company gathering data about wines from their app's users _Vivino_. Data are gathered in this repo in a SQL DataBase located in the `db/` folder.
+
+> The objective of this Repository is to query the database to give insights on the wine market and on __Wiwino__ business.
+
+
+## Installation
+To install __Wiwino__ market analysis script , follow these steps:
+
+- Clone the repository to your local machine using the command:
+```
+    git clone https://github.com/slvg01/7_wiwino.git
+```
+```
+    cd 7_wiwino
+```
+- Ensure that you have the required dependencies installed by executing:
+```
+    pip install -r requirements.txt
+```
+
+## How to use
+The Data Analysis is processed by running the `queries_exec.py` file that you will find in the root folder : `7_wiwino/` 
+
+The script is launching successively a list of 9 queries and shows the results right in the terminal __(ensure to open it widely :)__. 
+Each time, the result of the queries includes : 
+ - the query result itself in a Dataframe
+ - the query objectives, 
+ - its logic, 
+ - its conclusion, 
+ - and if relevant a graphical visualization  will open in a separate window
+
+When running the ``queries_exec.py file``, the script will initially: 
+- Execute the first queries (__queries0__) automatically. This query will display the market overview
+- In the terminal, it is then inviting the user to press enter to execute the second query
+- and so on until the last one. 
+
+The queries codes are located in the `db/`folder for your review. 
+
+## Main Conclussion 
+
+- Most common grapes in the world are Cabernet Sauvignon, Chardonnay and Pinot
+- Wine Market is dominated by France, Italy and US in terms of number of wineries and number of wines
+- German, Israel and US wines are the top rated one, worldwide
+- Some markets have some growth potential for __Wiwino__ app with high number of customers but a low level of engagement (Germany, US, switzerland)   Those 3 countries maybe quite interesting for a marketing push.
+
--- queries_exec.py
+++ queries_exec.py
@@ -5,70 +5,75 @@ import subprocess
 from PIL import Image
 
 
-
 # Connect to the SQLite database
-connexion = sqlite3.connect('db/vivino.db')
+connexion = sqlite3.connect("db/vivino.db")
 cursor = connexion.cursor()
 
 
-#List of SQL files
-sql_files = ['db/queries0.sql','db/queries1.sql', 'db/queries2.sql', 'db/queries3.sql', 'db/queries4a.sql','db/queries4b.sql', 'db/queries5a.sql','db/queries5b.sql', 'db/queries6.sql','db/queries7.sql']  
+# List of SQL files
+sql_files = [
+    "db/queries0.sql",
+    "db/queries1.sql",
+    "db/queries2.sql",
+    "db/queries3.sql",
+    "db/queries4a.sql",
+    "db/queries4b.sql",
+    "db/queries5a.sql",
+    "db/queries5b.sql",
+    "db/queries6.sql",
+    "db/queries7.sql",
+]
 
 
 for sql_file_name in sql_files:
-    
-# Read .sql query and print comments in it 
+    # Read .sql query and print comments in it
     sql_queries = ""
-    comments = ''
-    with open(sql_file_name, 'r') as sql_file:
+    comments = ""
+    with open(sql_file_name, "r") as sql_file:
         for line in sql_file:
             sql_queries += line
-            if line.strip().startswith('--'):
-                comments += line.strip()[2:].strip() + '\n'
-        
+            if line.strip().startswith("--"):
+                comments += line.strip()[2:].strip() + "\n"
 
     # Execute the SQL queries
     cursor.execute(sql_queries)
 
-
-    #Display results
+    # Display results
     df = pd.read_sql_query(sql_queries, connexion)
-    print('--------------------------------------------------------------------------------')
-    print('QUERY OUTPUT:')
+    print(
+        "--------------------------------------------------------------------------------"
+    )
+    print("QUERY OUTPUT:")
     print()
     print(df)
-    print(),print()
-    print(comments + 'You may have a look.')
     print(), print()
-    print('Proceed with the next query')
-    
+    print(comments + "You may have a look.")
+    print(), print()
+    print("Proceed with the next query")
 
-    # check if vizualisation is needed and launch it 
-    with open(sql_file_name, 'r') as sql_file:
+    # check if vizualisation is needed and launch it
+    with open(sql_file_name, "r") as sql_file:
         for line in sql_file:
             sql_queries += line
-            if line.strip().startswith('-- VISUALIZATION image is opening in a new window'):
+            if line.strip().startswith(
+                "-- VISUALIZATION image is opening in a new window"
+            ):
                 # Get the base name of the SQL file (without the extension)
                 base_name = os.path.splitext(sql_file_name)[0]
                 # Add the .png extension to the image
-                image_name = base_name + '.png'
+                image_name = base_name + ".png"
                 # Open an image file
-                subprocess.call(['code', image_name])
-                #img = Image.open(image_name)
+                subprocess.call(["code", image_name])
+                # img = Image.open(image_name)
                 # Display the image
-                #img.show()
+                # img.show()
             else:
                 continue
-                
-
 
     # Wait for the user to press a key before executing the next file
     input("Please press Enter...")
-    print(), print(), print(), print(),print()
+    print(), print(), print(), print(), print()
 
-    
 
 # Close the database connection
 connexion.close()
-
-
